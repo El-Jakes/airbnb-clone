e0f7a5a946f768e3586664213d9c9e047a919500
ElJakes <jidejacobs1@gmail.com>
2023-07-21 00:16:17 +0100

    modify app and cards component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Nav from './components/Nav'
 import Hero from './components/Hero'
 import Cards from './components/Cards';
+import image from './images/katie.png'
 
 function App() {
   return (
@@ -8,10 +9,10 @@ function App() {
       <Nav />
       <Hero />
       <Cards 
-        img="katie-zaferes"
+        img={image}
         rating="5.0"
         review={6}
-        cuuntry="USA"
+        country="USA"
         title="Life Lessons with Katie Zaferes"
         price={136}
       />
--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import katie from '../images/katie.png'
+// import katie from '../images/katie.png'
 import star from '../images/star.png'
 
 const Cards = (props) => {
@@ -9,13 +9,13 @@ const Cards = (props) => {
         <img className='card-img' src={props.img} alt="Katie Zaferes wearing a swim suit with a swim cap and goggles" />
         <div className='card-stats'>
             <img src={star} alt="a red star" className='card-star' />
-            <span>props.rating</span>
-            <span className='gray-tint'>{props.review} • </span>
+            <span>{props.rating}</span>
+            <span className='gray-tint'>({props.review}) • </span>
             <span className='gray-tint'>{props.country}</span> 
             
         </div>
         <p className='cardi-title'>{props.title}</p>
-        <p><span className='card-price'>From props.price</span> / person</p>
+        <p><span className='card-price'>From ${props.price}</span> / person</p>
         </div>
     </section>
   )
