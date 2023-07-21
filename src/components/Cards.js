ce2baf00d540b75d0f6174a12136f0ae7750cb4a
solo dev <solodev001@gmail.com>
2023-07-21 14:25:01 +0100

    fix a bug in card component

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,12 +1,11 @@
 import React from 'react'
-// import katie from '../images/katie.png'
 import star from '../images/star.png'
 
 const Cards = (props) => {
   return (
     <section>
         <div className='card'>
-        <img className='card-img' src={katie} alt="Katie Zaferes wearing a swim suit with a swim cap and goggles" />
+        <img className='card-img' src={props.img} alt="Katie Zaferes wearing a swim suit with a swim cap and goggles" />
         <div className='card-stats'>
             <img src={star} alt="a red star" className='card-star' />
             <span>{props.rating}</span>
