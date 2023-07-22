7233d6a6e305bc715d79fc741a4e9721446d2e01
ElJakes <jidejacobs1@gmail.com>
2023-07-22 15:01:30 +0100

    add comments for future reference

--- src/App.js
+++ src/App.js
@@ -10,6 +10,9 @@ function App() {
         key={datum.id}
         /*  Passing objects as props: this is to ensure our code is dry. 
             The datum object should also be passed in the Card.js component in order for this to work.
+
+            An alternative method is using the spread operator
+            e.g {...datum}. If i am using this, i'd have to remove the .data i added to the props in the Cards component 
         */ 
         datum={datum}
       />
