20ea6bab29576e7812fc5a825c203113ae691dbd
ElJakes <jidejacobs1@gmail.com>
2023-07-22 14:41:09 +0100

    refactor conditional rendered code

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -2,9 +2,16 @@ import React from "react";
 import star from "./images/star.png";
 
 const Cards = (props) => {
+  let badgeText
+  if (props.openSpots === 0) {
+    badgeText = 'SOLD OUT'
+  } else if (props.location === 'Online') {
+    badgeText = 'ONLINE'
+  }
+
   return (
     <div className="card">
-      {props.openSpots === 0 && <div className="card-badge">SOLD OUT</div>}
+      {badgeText && <div className="card-badge">{badgeText}</div>}
       <img
         className="card-img"
         src={props.coverImg}
